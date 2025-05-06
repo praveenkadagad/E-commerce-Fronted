0c59e222921b59f4a28c2868997384f06ea9c6ce
praveenkadagad <praveenkadagad18@gmail.com>
2025-05-06 16:04:04 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        id: 1,
+        title: "Wireless Headphones",
+        price: 2499,
+        description: "Noise Cancelling over-ear Bluetooth Headphones",
+        category: "Electronics",
+        image: "images/headphones.webp",
+    },
+    {
+        id: 2,
+        title: "Smart Watch",
+        price: 1599,
+        description: "Fitness Smart Watch with Heart Rate Monitor",
+        category: "Electronics",
+        image: "images/smartwatch.webp",
+    },
+    {
+        id: 3,
+        title: "T-shirt",
+        price: 399,
+        description: "100% Cotton Printed T-shirt",
+        category: "Clothing",
+        image: "images/mi t shrit.webp",
+    },
+    {
+        id: 4,
+        title: "Mobiles",
+        price: 22499,
+        description: "64 GB RAM and 256 GB storege ",
+        category: "Electronics",
+        image: "images/mi mobiles.jpg",
+    },
+   
+    {
+        id: 5,
+        title: "Laptop",
+        price: 67000,
+        description: "64 GB RAM and 512 GB SSD Memory Card ",
+        category: "Electronics",
+        image: "images/laptop.jpg",
+    }
+    // Add more product as needed
+];
